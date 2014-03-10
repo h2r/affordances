2749c84164a2d1dabeed842bd88415bbf468dab3
Dave Abel <dave.abel.cs@gmail.com>
2014-03-10 10:28:55 -0400

    furnace working

--- value_iter_bot/move.js
+++ value_iter_bot/move.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var mineflayer = require('../minecraft_api/mineflayer/');
 var vec3 = mineflayer.vec3;
+// var furnace = mineflayer.furnace;
 var bot = mineflayer.createBot({
   username: "aye_priori",
     password: "password",
@@ -46,6 +47,9 @@ bot.on('chat', function(username, message) {
     case "gm0":
       bot.chat("/gamemode 0");
       break;
+    case "bake":
+      bakeBread();
+      break;    
     case "r":
       bot.chat("/tp aye_priori 283 87 999");
       bot.chat("/time set 1000");
@@ -184,6 +188,8 @@ function execPlan(plan, step) {
     destroy("right");
   } else if (plan[step] == "pickUpGrain") {
     pickUpGrain();
+  } else if (plan[step] == "bakeBread") {
+    bakeBread();
   }
 
   // Clear control states
@@ -204,8 +210,8 @@ function pickUpGrain() {
   equip("pickaxe")
   b1 = bot.blockAt(bot.entity.position.offset(1,0,0));
   b2 = bot.blockAt(bot.entity.position.offset(-1,0,0));
-  b3 = bot.blockAt(bot.entity.position.offset(0,1,0));
-  b4 = bot.blockAt(bot.entity.position.offset(0,-1,0));
+  b3 = bot.blockAt(bot.entity.position.offset(0,0,1));
+  b4 = bot.blockAt(bot.entity.position.offset(0,0,-1));
 
   if (b1.name == "oreGold") {
     destroy("right");
@@ -219,6 +225,110 @@ function pickUpGrain() {
     console.log("Not near grain " + bot.entity.position);
   }
 }
+
+function bakeBread() {
+  equip("ore")
+
+  b1 = bot.blockAt(bot.entity.position.offset(1,0,0));
+  b2 = bot.blockAt(bot.entity.position.offset(-1,0,0));
+  b3 = bot.blockAt(bot.entity.position.offset(0,0,1));
+  b4 = bot.blockAt(bot.entity.position.offset(0,0,-1));
+
+  if (b1.name == "furnace") {
+    bot.openFurnace(b1);
+    } else if (b2.name == "furnace") {
+    bot.openFurnace(b2);
+  } else if (b3.name == "furnace") {
+    bot.openFurnace(b3);
+  } else if (b4.name == "furnace") {
+    // bot.openFurnace(b4);
+    watchFurnace(b4);
+  } else {
+    console.log("Not near furnace " + b1.name + " " + b2.name + " " + b3.name + " " + b4.name);
+  }
+}
+
+function itemStr(item) {
+  if (item) {
+    return item.name + " x " + item.count;
+  } else {
+    return "(nothing)";
+  }
+}
+
+ function watchFurnace(furnaceBlock) {
+  // var furnaceBlock = findFurnace();
+  if (! furnaceBlock) {
+    bot.chat("no furnace found");
+    return;
+  }
+  var furnace = bot.openFurnace(furnaceBlock);
+  furnace.on('open', function() {
+    var output = "";
+    output += "input: " + itemStr(furnace.inputItem()) + ", ";
+    output += "fuel: " + itemStr(furnace.fuelItem()) + ", ";
+    output += "output: " + itemStr(furnace.outputItem());
+    bot.chat(output);
+  });
+  furnace.on('updateSlot', function(oldItem, newItem) {
+    // bot.chat("furnace update: " + itemStr(oldItem) + " -> " + itemStr(newItem));
+  });
+  furnace.on('close', function() {
+    // bot.chat("furnace closed");
+  });
+  furnace.on('update', function() {
+    // console.log("fuel: " + Math.round(furnace.fuel * 100) +
+      // "% progress: " + Math.round(furnace.progress * 100) + "%");
+  });
+
+  bot.on('chat', onChat);
+  
+  function onChat(username, message) {
+    equip("ore");
+    var words, cmd, name, amount, item, fn;
+    if (message === 'close') {
+      furnace.close();
+      bot.removeListener('chat', onChat);
+    } else if (/^(input) (\d+)/.test(message)) {
+      words = message.split(/\s+/);
+      cmd = words[0];
+      amount = parseInt(words[1], 10);
+      name = words[2];
+      item = itemByName("oreGold");
+      if (!item) {
+        bot.chat("unknown item " + name);
+        return;
+      }
+      console.log("MONEY SPOT");
+      fn = cmd === 'input' ? furnace.putInput : furnace.putFuel;
+      fn.call(furnace, item.type, null, amount, function(err) {
+        if (err) {
+          // bot.chat("unable to put " + amount + " " + item.name);
+        } else {
+          bot.chat("put " + amount + " " + item.name);
+          furnace.close();
+
+        }
+      });
+    } else if (/^take (input|fuel|output)$/.test(message)) {
+      words = message.split(/\s+/);
+      cmd = words[1];
+      fn = {
+        input: furnace.takeInput,
+        fuel: furnace.takeFuel,
+        output: furnace.takeOutput,
+      }[cmd];
+      fn.call(furnace, function(err, item) {
+        if (err) {
+          // bot.chat("unable to take " + item.name + " from " + cmd);
+        } else {
+          bot.chat("took " + item.name + " " + cmd);
+        }
+      });
+    }
+  }
+}
+
 
 function jump(dir) {
   bot.setControlState(dir, true);
@@ -373,6 +483,9 @@ function equip(itemName) {
     bot.equip(item,'hand',null);
   } else if (itemName == "block") {
     item = itemByName("cloth");
+    bot.equip(item,'hand',function(err){});
+  } else if (itemName == "ore") {
+    item = itemByName("oreGold");
     bot.equip(item,'hand',function(err){});
   }
 }
