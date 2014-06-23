0c6436ddbeefd5fd7b0669fc3149cdc2ff6e2945
DHershko <Dhershkowitz@Davids-MacBook-Pro-5.local>
2014-06-23 10:57:39 -0400

    Big Update (pre-cleanup)
    Implemented the following since last commit:
    
    Agent destroying blocks
    
    Agent rotating along a horizontal axis
    
    Agent placing blocks
    
    Block dropping dirt items which can then be picked up and placed
    
    Obscuring agent’s view when necessary so he/she/it cannot pickup or
    destroy blocks through another block
    
    Did some other refactoring to keep java classes small
    
    Implemented two new propositional functions: 1) BlockAtPF which checks
    if there is a block at a specified location — used to get agent to plan
    to place a block somewhere. 2) EmptySpacePF which checks if there is no
    block at a specified location — used to get agent to destroy a block
    somewhere.
    
    Added an agent’s feet burlap agent that moves with the agent so there
    aren’t a bunch of special-cased checks on both the agent’s z and z-1
    
    And other stuff!

--- src/NewMineCraftDomain/src/minecraft/NameSpace.java
+++ src/NewMineCraftDomain/src/minecraft/NameSpace.java
@@ -2,14 +2,20 @@ package minecraft;
 
 public class NameSpace {
 	//-------------ATTRIBUTE STRINGS-------------
-	public static final String							ATTX = "x";
-	public static final String							ATTY = "y";
-	public static final String							ATTZ = "z";
-	public static final String							ATTCOLLIDESWITHAGENT = "collides";
-	
+	public static final String							ATX = "x";
+	public static final String							ATY = "y";
+	public static final String							ATZ = "z";
+	public static final String							ATCOLLIDES = "collides";
+	public static final String 							ATROTDIR = "rotationalDirection";
+	public static final String							ATDEST = "destroyable";
+	public static final String							ATFLOATS = "floats";
+	public static final String							ATPLACEBLOCKS = "placeableBlocks";
+	public static final String							ATVERTDIR = "verticalDirection";
+	public static final String							ATDESTWHENWALKED = "destroyedByAgentWhenWalkedOn";
 	
 	//-------------BURLAP OBJECTCLASS STRINGS-------------
 	public static final String							CLASSAGENT = "agent";
+	public static final String							CLASSAGENTFEET = "agentsFeet";
 	public static final String							CLASSGOAL = "goal";
 	public static final String							CLASSDIRTBLOCK = "dirtblock";
 
@@ -19,19 +25,83 @@ public class NameSpace {
 	public static final char AGENT = 'a';
 	public static final char DIRTBLOCK = '.';
 	public static final char BLOCKEMPTY = 'e';
-	
+	public static final char AGENTFEET = 'f';
+	public static final char PLACEABLEBLOCKS = 'B';
 	
 	//-------------ACTIONS STRINGS-------------
-	public static final String							ACTIONNORTH = "north";
-	public static final String							ACTIONSOUTH = "south";
-	public static final String							ACTIONEAST = "east";
-	public static final String							ACTIONWEST = "west";
+	public static final String							ACTIONMOVE = "moveAction";
+	public static final String							ACTIONROTATEC = "rotateClockwise";
+	public static final String 							ACTIONROTATECC = "rotateCounterClockwise";
+	public static final String 							ACTIONDESTBLOCK = "destroyBlock";
 	public static final String							ACTIONJUMP = "jump";
-	
+	public static final String							ACTIONPLACEBLOCK = "placeBlock";
+	public static final String							ACTIONLOOKUP = "lookup";
+	public static final String							ACTIONLOOKDOWN = "lookdown";
 	
 	//-------------PROPOSITIONAL FUNCTION STRINGS-------------
 	public static final String					PFATGOAL = "AtGoal";
+	public static final String					PFEMPSPACE = "Empty space";
+	public static final String					PFBLOCKAT = "block at";
 	
+	//ENUMS
+	public enum RotDirection {
+		NORTH(0), EAST(1), SOUTH(2), WEST(3);
+		public static final int size = RotDirection.values().length;
+		private final int intVal;
+		
+		RotDirection(int i) {
+			this.intVal = i;
+		}
+		
+		public int toInt() {
+			return this.intVal;
+		}
+		
+		public static RotDirection fromInt(int i) {
+			switch (i) {
+			case 0:
+				return NORTH;
+			case 1:
+				return EAST;
+			case 2:
+				return SOUTH;
+			case 3:
+				return WEST;
+			
+			}
+			return null;
+		}
+		
+	}
 	
-	
+	public enum VertDirection {
+		AHEAD(3), DOWNONE(2), DOWNTWO(1), DOWNTHREE(0);
+		public static final int size = VertDirection.values().length;
+		private final int intVal;
+		
+		VertDirection(int i) {
+			this.intVal = i;
+		}
+		
+		public int toInt() {
+			return this.intVal;
+		}
+		
+		public static VertDirection fromInt(int i) {
+			switch (i) {
+			case 3:
+				return AHEAD;
+			case 2:
+				return DOWNONE;
+			case 1:
+				return DOWNTWO;
+			case 0:
+				return DOWNTHREE;
+			
+			}
+			return null;
+		}
+		
+	}
+
 }
