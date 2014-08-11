b44fcab7abe2d59fe74589d4253897d175ecc1f3
Dave Abel <dave.abel.cs@gmail.com>
2014-08-11 13:23:30 -0400

    adding grid stuff

---
+++ grid/run_grid_test.py
@@ -0,0 +1,3 @@
+#!/bin/python
+
+subprocess.call(['java', '-jar', 'minecraftTests.jar'], env=env)
---
+++ grid/run_tests.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+import subprocess
+subprocess.call(['java', '-jar', 'minecraftTests.jar'])
