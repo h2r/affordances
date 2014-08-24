66c4bb3c419bb57274a6e3ed3db4129152671a71
Dave Abel <dave.abel.cs@gmail.com>
2014-08-24 16:28:50 -0400

    Grid setup done

--- grid/run_tests.py
+++ grid/run_tests.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
 import subprocess
-subprocess.call(['java', '-jar', '-Xmx1024m', 'minecraftTests.jar'])
+subprocess.call(['java', '-jar', '-Xmx1024m', 'minecraftLearner.jar'])
