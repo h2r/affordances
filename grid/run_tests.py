fd9b59d099562cac3c41d51c9b7ab19363a2c905
Dave Abel <dave.abel.cs@gmail.com>
2014-08-25 17:18:19 -0400

    learning rate jar

--- grid/run_tests.py
+++ grid/run_tests.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
 import subprocess
-subprocess.call(['java', '-jar', '-Xmx2048m', 'minecraftTests.jar', '1'])
+subprocess.call(['java', '-jar', '-Xmx2048m', 'minecraftLearningRateTests.jar', '1'])
