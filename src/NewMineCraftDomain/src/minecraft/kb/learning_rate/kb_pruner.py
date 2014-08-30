0b142dadbbf56f559b100c26acb55e7a77ccb73b
Dave Abel <dave.abel.cs@gmail.com>
2014-08-30 15:36:07 -0400

    kb pruner finished

--- src/NewMineCraftDomain/src/minecraft/kb/learning_rate/kb_pruner.py
+++ src/NewMineCraftDomain/src/minecraft/kb/learning_rate/kb_pruner.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from os import listdir, rename
 from os.path import isfile, join
 
@@ -13,7 +14,12 @@ for f in all_kbs:
 
 
 for kb in kb_files:
-	out_file = file("lr_" + kb[-2:] + ".kb", "w+")
+	oldfract = re.sub("[^0-9]", "", kb)
+	if(len(oldfract) < 1):
+		continue
+	newfract = float(oldfract) / 20.0 - 0.05
+	new_kb_name = "lr_" + "{0:.2f}".format(newfract) + ".kb"
+	out_file = file(new_kb_name, "w+")
 	old_file = file(kb,"r")
 	for line in old_file.readlines():
 		if len(line) <= 1:
