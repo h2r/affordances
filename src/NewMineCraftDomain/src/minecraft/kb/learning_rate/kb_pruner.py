30261d9324a368bdfc5678b32f9bfbdca4061536
dave-abel <dave.abel.cs@gmail.com>
2014-08-30 15:08:52 -0400

    new kb pruner

--- src/NewMineCraftDomain/src/minecraft/kb/learning_rate/kb_pruner.py
+++ src/NewMineCraftDomain/src/minecraft/kb/learning_rate/kb_pruner.py
@@ -1,30 +1,24 @@
-import sy
-from os import listdir
+import sys
+from os import listdir, rename
 from os.path import isfile, join
 
 path = "."
 
-all_kbs = [f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+all_kbs = [f for f in listdir(path) if isfile(join(path,f)) ]
 kb_files = []
 
 # Get kb files
-for f in all_files:
-	if ".kb" in f and "old" not in f:
-		kb_files.append(f)
+for f in all_kbs:
+	kb_files.append(f)
 
-# Rename all files
+
 for kb in kb_files:
-	os.rename(kb, "old_" + kb)
-
-
-out_file = file("new_" + sys.argv[1], "w+")
-
-for line in f.readlines():
-	if len(line) <= 1:
-		print line
-		continue
-	elif ";" in line:
-		print line
-		continue
-	else:
-		out_file.write(line)
+	out_file = file("lr_" + kb[-2:] + ".kb", "w+")
+	old_file = file(kb,"r")
+	for line in old_file.readlines():
+		if len(line) <= 1:
+			continue
+		elif ";" in line:
+			continue
+		else:
+			out_file.write(line)
